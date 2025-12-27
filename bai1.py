c9065d3c0b82382ca96453048db1f95abe4011a3
coding is too ez <itskobikjca@gmail.com>
2025-12-27 23:00:34 +0700

    Add files via upload
    27/12/25

--- bai1.py
+++ bai1.py
@@ -1,36 +1,43 @@
-# Import widgets from guizero
-from guizero import App, Text, PushButton, Box
+# Import GUI essentials
+from guizero import App, Text, Box, TextBox, Combo, ButtonGroup, PushButton, info # + tenary operator
 
-# Initiate app
-window = App(title = "Quiz App", width = 300, height = 200, layout = "grid")
-window.bg = "white"
+# Function
+def display():
+    global name_output, class_output, gender_output
+    global name_entry, class_entry, gender_entry
+    
+    info(title = "Notification", text = "You must enter a name!") if name_entry.value == "" else text_assigning()
 
-# Header box + quiz
-header = Box(window, layout = "grid", grid = [0, 0])
-q = Text(header, text = "What is the capital of Vietnam?", grid = [0, 0])
+def text_assigning():
+    global name_output, class_output, gender_output
+    global name_entry, class_entry, gender_entry
 
-# Answer + logic check (if answer correct -> bg turns green and vice versa for wrong -> red)
-answer_box = Box(window, align = "bottom", grid = [0, 1], layout = "grid")
+    name_output.value = name_entry.value
+    class_output.value = class_entry.value
+    gender_output.value = gender_entry.value
 
-def logic_check(ans_btn : PushButton):
-    if ans_btn.text == "Hanoi":
-        return True
-    else:
-        return False
+    
 
-def change_screen_color(ans_btn : PushButton):
-    if logic_check(ans_btn) == True:
-        window.bg = "green"
-    elif logic_check(ans_btn) == False:
-        window.bg = "red"
-real_btn_1 = PushButton(answer_box, text = "Hanoi", grid = [0, 0]) 
-real_btn_2 = PushButton(answer_box, text = "Danang City", grid = [0, 1])
-real_btn_3 = PushButton(answer_box, text = "Saigon City", grid = [0, 2])
+# GUI
+window = App(title = "Display information")
 
-# Update the command for each one
-real_btn_1.update_command(command = change_screen_color, args = [real_btn_1])
-real_btn_2.update_command(command = change_screen_color, args = [real_btn_2])
-real_btn_3.update_command(command = change_screen_color, args = [real_btn_3])
+# Info entry box
+entry_box = Box(window, layout = "grid")
+txt1 = Text(entry_box, text = "Full name: ", grid = [0 ,0])
+name_entry = TextBox(entry_box, width = 30, grid = [1, 0])
+txt2 = Text(entry_box, text = "Class: 10A", grid = [0, 1])
+class_entry = Combo(entry_box, grid =[1, 1], options = [i for i in range(1, 16)])
+txt3 = Text(entry_box, text = "Gender: ", grid =[0, 2])
+gender_entry = ButtonGroup(entry_box, grid =[1, 2], options =["Male", "Female", "Other"])
 
-# Run app
+display_btn = PushButton(window, command = display, width = 10, height = 1, text = "Display")
+
+display_box = Box(window, layout = "grid")
+tx4 = Text(display_box, grid = [0, 0], text = txt1.value)
+name_output = Text(display_box, grid = [1, 0], text = "")
+txt5 = Text(display_box, grid = [0, 1], text = txt2.value)
+class_output = Text(display_box, grid = [1, 1], text = "")
+txt6 = Text(display_box, grid = [0, 2], text = txt3.value)
+gender_output = Text(display_box, grid = [1, 2], text = "")
+
 window.display()
